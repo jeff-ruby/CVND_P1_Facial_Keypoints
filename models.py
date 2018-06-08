8e47db7ff365aa2a0b4fe42c26523dd7c0f5b3a4
Ubuntu <ubuntu@ip-172-31-46-4.ec2.internal>
2018-06-08 15:48:50 +0000

    finetune

--- models.py
+++ models.py
@@ -25,13 +25,25 @@ class Net(nn.Module):
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         self.bn1 = nn.BatchNorm2d(32)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.AvgPool2d(2, 2)
 
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, 4, padding=0)
         self.bn2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=0)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        self.conv4 = nn.Conv2d(128, 256, 2, padding=0)
+        self.bn4 = nn.BatchNorm2d(256)
+        
+        self.conv5 = nn.Conv2d(256, 384, 1, padding=0)
+        self.bn4_1 = nn.BatchNorm2d(384)
 
-        self.fc = nn.Linear(186624, 136)
-
+        self.fc1 = nn.Linear(13824, 1000)
+        self.bn5 = nn.BatchNorm1d(1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.bn6 = nn.BatchNorm1d(1000)
+        self.fc3 = nn.Linear(1000, 136)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
@@ -45,12 +57,32 @@ class Net(nn.Module):
         # conv2
         x = self.bn2(self.conv2(x))
         x = self.pool(F.relu(x))
+        
+        # conv3
+        x = self.bn3(self.conv3(x))
+        x = self.pool(F.relu(x))
+
+        # conv4
+        x = self.bn4(self.conv4(x))
+        x = self.pool(F.relu(x))
+        
+        # conv5
+        x = self.bn4_1(self.conv5(x))
+        x = self.pool(F.relu(x))
 
         # flatten
         x = x.view(x.size()[0], -1)
 
-        # fc
-        x = self.fc(x)
+        # fc1
+        x = self.bn5(self.fc1(x))
+        x = F.relu(x)
+        
+        # fc2
+        x = self.bn6(self.fc2(x))
+        x = F.relu(x)
+        
+        # fc3
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
