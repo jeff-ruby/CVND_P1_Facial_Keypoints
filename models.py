e9106ecef62964ecc60c0f485882f7b75ca8eeac
Jeff Li <dalee_xd@hotmail.com>
2018-06-08 21:28:09 +0800

    tuning

--- models.py
+++ models.py
@@ -20,11 +20,17 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5, padding=0)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=0)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.fc = nn.Linear(186624, 136)
 
         
     def forward(self, x):
@@ -32,6 +38,19 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        # conv1
+        x = self.bn1(self.conv1(x))
+        x = self.pool(F.relu(x))
+
+        # conv2
+        x = self.bn2(self.conv2(x))
+        x = self.pool(F.relu(x))
+
+        # flatten
+        x = x.view(x.size()[0], -1)
+
+        # fc
+        x = self.fc(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
